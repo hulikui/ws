e49fe20f252e02aa078199f7bc87902547d08119
root <root@localhost>
2016-04-28 18:57:13 +0800

    111

--- weixin.js
+++ weixin.js
@@ -14,16 +14,19 @@
 */
 
 var PORT = require('./lib/config').wxPort;
-
+var fs=require('fs');
 var http = require('http');
 var qs = require('qs');
-var TOKEN = 'yuntu';
-
-var getUserInfo = require('./lib/user').getUserInfo;
-var replyText = require('./lib/reply').replyText; 
-
+var TOKEN = 'hulikui';
+var request = require('request');
+var getToken = require('./lib/token').getToken;
+var appID = require('./lib/config').appID;
+var appSecret = require('./lib/config').appSecret;
+var replyImg = require('./lib/reply').replyImg; 
 var wss = require('./lib/ws.js').wss;
-
+var getUserInfo=require('./lib/user').getUserInfo;
+var getImage=require('./lib/user').getImage;
+var replyText=require('./lib/reply').replyText;
 function checkSignature(params, token){
   //1. 将token、timestamp、nonce三个参数进行字典序排序
   //2. 将三个参数字符串拼接成一个字符串进行sha1加密
@@ -62,19 +65,32 @@ var server = http.createServer(function (request, response) {
     //获取到了POST数据
     request.addListener("end",function(){
       var parseString = require('xml2js').parseString;
-
       parseString(postdata, function (err, result) {
         if(!err){
-          if(result.xml.MsgType[0] === 'text'){
+		console.log(result);
+        if(result.xml.MsgType[0] === 'text'){
             getUserInfo(result.xml.FromUserName[0])
             .then(function(userInfo){
               //获得用户信息，合并到消息中
               result.user = userInfo;
               //将消息通过websocket广播
               wss.broadcast(result);
-              var res = replyText(result, '消息推送成功！');
+              var res = replyText(result, '文字消息推送成功！');
+             	console.log(res);
+		 response.end(res);
+            })}
+       else if(result.xml.MsgType[0] === 'image'){
+            getUserInfo(result.xml.FromUserName[0])
+            .then(function(userInfo){
+              //获得用户信息，合并到消息中
+              result.user = userInfo;
+              //将消息通过websocket广播
+              wss.broadcast(result);
+              var res = replyImg(result);
+		console.log(res);
               response.end(res);
             })
+	getImage(result.xml.MediaId[0]);
           }
         }
       });
