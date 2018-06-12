8b7defd826dbc0c8be5a3cc8679d811d58450efc
likuihu <likuihu@likuihudeMacBook-Pro.local>
2018-06-12 12:26:58 +0800

    beiyou bbs network

--- scheduleCronstyle.js
+++ scheduleCronstyle.js
@@ -6,18 +6,22 @@ var cheerio = require('cheerio');
 var schedule = require('node-schedule');
 const readline = require('readline');
 var process = require('child_process');
-
+request = request.defaults({jar: true})
+var base_bj_url = 'http://bbs.cloud.icybee.cn';
+var yingjisheng_url = 'http://s.yingjiesheng.com/search.php?word=%E5%89%8D%E7%AB%AF%E6%88%B7%E5%8F%A3&area=1056&jobterm=0&sort=date&start=';
+var beiyou_url = 'http://bbs.cloud.icybee.cn/board/Job?p='
 let res = [];
 
 /* 过滤页面信息 */
 function filterSlideList(html) {
+    var slideListData = [];
+
     if (html) {
         // 沿用JQuery风格，定义$
         var $ = cheerio.load(html);
         // 根据id获取轮播图列表信息
         var slideList = $('.searchResult');
         // 轮播图数据
-        var slideListData = [];
         /* 轮播图列表信息遍历 */
         slideList.find('li').each(function(item) {
             var art = $(this);
@@ -33,33 +37,70 @@ function filterSlideList(html) {
             });
         });
         // 返回轮播图列表信息
-        return slideListData;
     } else {
         console.log('无数据传入！');
     }
+    return slideListData;
+
 }
 
+function filterBYSideList(html) {
+    var slideListData = [];
+    if (html) {
+        var $ = cheerio.load(html);
+        var sideList = $('.list.sec');
+        sideList.find('li').each(function(item) {
+            var art = $(this);
+            // 找到a标签并获取href属性
+            var titleDom = art.find('div:first-child a');
+            var href = base_bj_url + titleDom.attr('href');
+            var title = titleDom.text();
+            var time = art.find('div:last-child').text();
+            // 向数组插入数据
+            var time_pattern = /[0-9]{4}-[0-9]{2}-[0-9]{2}/;
+            if (title.indexOf('户口') >= 0 && title.indexOf('违约') < 0) {
+                console.log(time);
+                slideListData.push({
+                    title : title,
+                    href : href,
+                    time : time.match(time_pattern) && time.match(time_pattern)[0] || ''
+                });
+            }
+        });
+    } else {
+        console.log('bbs 数据为空');
+    }
+    return slideListData;
+}
 function writeFile(data, output) {
     fs.writeFileSync('./output/filelist.json', JSON.stringify(data), {encoding: 'utf-8'});
 }
 
-var queue = async.queue(function (url, callback) {
-    request({url: url, timeout: 3000}, function (error, response, body) {
-        let message = '';
-        if (response && response.statusCode > 300) {//防止网页超时或者跳转无法访问的情况
-            message = `问题URL: ${response && response.statusCode + '\n' + url}`
-        }
-        if (!error) {
-            let sildeList = filterSlideList(body);
-            res = res.concat(sildeList);
-            writeFile(res);
-            message = '处理成功'
-        } else {
-            message = `错误: , ${response && response.statusCode + '\n' + url + error.message}`
-        }
-        callback(message)
-    })
-}, 1); // 并发 5
+var queue = async.queue(function (options, callback) {
+        console.log(options);
+        request(options, function (error, response, body) {
+            let message = '';
+            if (response && response.statusCode > 300) {//防止网页超时或者跳转无法访问的情况
+                message = `问题URL: ${response && response.statusCode + '\n' + url}`
+            }
+            if (!error) {
+                //console.log(body);
+                let sildeList = [];
+                if (options.url.indexOf(base_bj_url) >=0) {
+                    sildeList = filterBYSideList(body);
+                } else {
+                    sildeList = filterSlideList(body);
+                }
+                res = res.concat(sildeList);
+                writeFile(res);
+                message = '处理成功'
+            } else {
+                message = `错误: , ${response && response.statusCode + '\n' + url + error.message}`
+            }
+            callback(message)
+        })
+
+}, 1); // 并发 1
 
 // var queue = async.queue(function (url, callback) {
 //     fs.readFile(`./resource/${url}.html`, {encoding: 'utf-8'}, function (error, body) {
@@ -86,13 +127,28 @@ function start() {
         console.log('新建output文件夹');
         process.exec('mkdir output');
     }
-    let url = `http://s.yingjiesheng.com/search.php?word=%E5%89%8D%E7%AB%AF%E6%88%B7%E5%8F%A3&area=1056&jobterm=0&sort=date&start=`;
-    [0, 1, 2].map(num => {
-        queue.push(url + num * 10, function (message) {
-            console.log(message)
-        });
+    [beiyou_url, yingjisheng_url].map(function (url) {
+        [0, 1, 2].map(num => {
+            let byoptinons = {
+                url: url + (num +1),
+                timeout: 3000,
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
+                }
+            };
+            let yjsoptions = {
+                url: url + num * 10,
+                timeout: 3000,
+            }
+            let options = yjsoptions;
+            if (beiyou_url === url) {
+                options = byoptinons;
+            }
+            queue.push(options, function (message) {
+                console.log(message)
+            });
+        })
     })
-
 }
 
 function scheduleCronstyle(){
@@ -101,9 +157,11 @@ function scheduleCronstyle(){
         start();
     });
 }
-
+//
+// var body = fs.readFileSync(path.join(__dirname, 'projects/test.html'), {encoding: 'utf-8'});
+// filterBYSideList(body)
+//start();
 module.exports = scheduleCronstyle
-
 
 
 
